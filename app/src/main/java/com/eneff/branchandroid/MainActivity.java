b229087b35a3e4d3f450a22e68dc27510bf7ecd3
Ethan Neff <ethan.neff@eneff.com>
2017-03-09 11:31:05 -0800

    added read and create link

--- app/src/main/java/com/eneff/branchandroid/MainActivity.java
+++ app/src/main/java/com/eneff/branchandroid/MainActivity.java
@@ -3,18 +3,25 @@ package com.eneff.branchandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
 import org.json.JSONObject;
 
+import java.util.Calendar;
+
+import io.branch.indexing.BranchUniversalObject;
 import io.branch.referral.Branch;
 import io.branch.referral.BranchError;
+import io.branch.referral.SharingHelper;
+import io.branch.referral.util.LinkProperties;
+import io.branch.referral.util.ShareSheetStyle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,12 +32,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // button click
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Branch Share Link
+                branchShareSheet();
             }
         });
     }
@@ -68,6 +76,16 @@ public class MainActivity extends AppCompatActivity {
             public void onInitFinished(JSONObject referringParams, BranchError error) {
                 if (error == null) {
                     Log.i("BRANCH SDK", referringParams.toString());
+
+                    // Branch Link Data Display (optional)
+                    try {
+                        String formattedText = referringParams.toString(2);
+                        TextView textView = (TextView)findViewById(R.id.contentTextView);
+                        textView.setMovementMethod(new ScrollingMovementMethod());
+                        textView.setText(formattedText);
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
                 } else {
                     Log.i("BRANCH SDK", error.getMessage());
                 }
@@ -78,6 +96,54 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onNewIntent(Intent intent) {
         this.setIntent(intent);
+    }
+
+    public void branchShareSheet() {
+        // Branch Share Link (optional)
+        BranchUniversalObject branchUniversalObject = new BranchUniversalObject()
+                .setCanonicalIdentifier("item/12345")
+                .setTitle("My Content Title")
+                .setContentDescription("My Content Description")
+                .setContentImageUrl("https://lorempixel.com/400/400")
+                .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+                .addContentMetadata("custom_data", "123")
+                .addContentMetadata("relate-to", "content");
+
+        LinkProperties linkProperties = new LinkProperties()
+                .setChannel("facebook")
+                .setFeature("sharing")
+                .setCampaign("content 123 launch")
+                .setStage("new user")
+                .addControlParameter("$deeplink_path", "content/123")
+                .addControlParameter("$desktop_url", "http://example.com/home")
+                .addControlParameter("custom", "data")
+                .addControlParameter("related-to", "link")
+                .addControlParameter("custom_random", Long.toString(Calendar.getInstance().getTimeInMillis()));
+
+        ShareSheetStyle shareSheetStyle = new ShareSheetStyle(MainActivity.this, "Check this out!", "This stuff is awesome: ")
+                .setCopyUrlStyle(ContextCompat.getDrawable(this, android.R.drawable.ic_menu_send), "Copy", "Added to clipboard")
+                .setMoreOptionStyle(ContextCompat.getDrawable(this, android.R.drawable.ic_menu_search), "Show more")
+                .addPreferredSharingOption(SharingHelper.SHARE_WITH.FACEBOOK)
+                .addPreferredSharingOption(SharingHelper.SHARE_WITH.EMAIL)
+                .addPreferredSharingOption(SharingHelper.SHARE_WITH.MESSAGE)
+                .addPreferredSharingOption(SharingHelper.SHARE_WITH.HANGOUT)
+                .setAsFullWidthStyle(true)
+                .setSharingTitle("Share With");
+
+        branchUniversalObject.showShareSheet(this, linkProperties,  shareSheetStyle,  new Branch.BranchLinkShareListener() {
+            @Override
+            public void onShareLinkDialogLaunched() {
+            }
+            @Override
+            public void onShareLinkDialogDismissed() {
+            }
+            @Override
+            public void onLinkShareResponse(String sharedLink, String sharedChannel, BranchError error) {
+            }
+            @Override
+            public void onChannelSelected(String channelName) {
+            }
+        });
     }
 
 }
